79abf59b212db5b3355257ebe6a9ed1c307fc672
mvrlrd <ontheinternets@yandex.ru>
2018-12-16 16:43:46 +0300

    в зависимости от уровня появляется больше врагов

--- core/src/ru/ibelykh/game/utils/EnemiesEmitter.java
+++ core/src/ru/ibelykh/game/utils/EnemiesEmitter.java
@@ -12,26 +12,26 @@ import ru.ibelykh.game.sprite.Enemy;
 public class EnemiesEmitter {
     private static final float ENEMY_SMALL_HEIGHT = 0.05f;
     private static final float ENEMY_SMALL_BULLET_HEIGHT = 0.01f;
-    private static final float ENEMY_SMALL_BULLET_VY = -0.5f;
+    private static final float ENEMY_SMALL_BULLET_VY = -0.3f;
     private static final int ENEMY_SMALL_BULLET_DAMAGE = 1;
-    private static final float ENEMY_SMALL_RELOAD_INTERVAL = 1f;
-    private static final int ENEMY_SMALL_HP = 3;
+    private static final float ENEMY_SMALL_RELOAD_INTERVAL = 2f;
+    private static final int ENEMY_SMALL_HP = 1;
     private static final int ENEMY_SMALL_Damage = 3;
 
     private static final float ENEMY_MEDIUM_HEIGHT = 0.1f;
     private static final float ENEMY_MEDIUM_BULLET_HEIGHT = 0.01f;
-    private static final float ENEMY_MEDIUM_BULLET_VY = -0.5f;
+    private static final float ENEMY_MEDIUM_BULLET_VY = -0.3f;
     private static final int ENEMY_MEDIUM_BULLET_DAMAGE = 5;
     private static final float ENEMY_MEDIUM_RELOAD_INTERVAL = 3f;
-    private static final int ENEMY_MEDIUM_HP = 10;
+    private static final int ENEMY_MEDIUM_HP = 2;
     private static final int ENEMY_MEDIUM_Damage = 10;
 
     private static final float ENEMY_BIG_HEIGHT = 0.125f;
     private static final float ENEMY_BIG_BULLET_HEIGHT = 0.015f;
-    private static final float ENEMY_BIG_BULLET_VY = -0.4f;
+    private static final float ENEMY_BIG_BULLET_VY = -0.3f;
     private static final int ENEMY_BIG_BULLET_DAMAGE = 10;
     private static final float ENEMY_BIG_RELOAD_INTERVAL = 5f;
-    private static final int ENEMY_BIG_HP = 15;
+    private static final int ENEMY_BIG_HP = 3;
     private static final int ENEMY_BIG_Damage = 15;
 
     private static final float DROP_BOX_HEIGHT = 0.07f;
@@ -44,7 +44,7 @@ public class EnemiesEmitter {
 
 
     private Rect worldBounds;
-    private float generateInterval = 4f;
+    private float generateInterval = 3f;
     private float generateTimer;
 
     private TextureRegion[] enemySmallRegion;
@@ -53,9 +53,9 @@ public class EnemiesEmitter {
     private TextureRegion[] dropBoxRegion;
 
 
-    private  final Vector2 enemySmallV = new Vector2(0f, -0.2f);
-    private  final Vector2 enemyMediumV = new Vector2(0f, -0.05f);
-    private  final Vector2 enemyBigV = new Vector2(0f, -0.06f);
+    private  final Vector2 enemySmallV = new Vector2(0f, -0.15f);
+    private  final Vector2 enemyMediumV = new Vector2(0f, -0.04f);
+    private  final Vector2 enemyBigV = new Vector2(0f, -0.02f);
     private  final Vector2 dropBoxV = new Vector2(0f, -0.5f);
 
     private int level;
@@ -86,7 +86,18 @@ public class EnemiesEmitter {
     }
 
     public void generate(float delta, int frags) {
-        level = frags / 20+1;
+        level = frags / 10+1;
+if (level<=20) {
+    generateInterval = 2f - ((float) level / 10) + 0.2f;
+}
+if ((level>20)&&(level<=40)){
+    generateInterval = 2f - ((float) level / 20) + 0.2f;
+
+}
+if (level>40){
+    generateInterval = 2f;
+}
+
         generateTimer += delta;
         if (generateTimer >= generateInterval) {
             generateTimer = 0f;
@@ -100,11 +111,11 @@ public class EnemiesEmitter {
                         bulletRegion,
                         ENEMY_SMALL_BULLET_HEIGHT,
                         ENEMY_SMALL_BULLET_VY,
-                        ENEMY_SMALL_BULLET_DAMAGE*level,
+                        ENEMY_SMALL_BULLET_DAMAGE,
                         ENEMY_SMALL_RELOAD_INTERVAL,
                         ENEMY_SMALL_HEIGHT,
-                        ENEMY_SMALL_HP*level,
-                        ENEMY_SMALL_Damage*level
+                        ENEMY_SMALL_HP,
+                        ENEMY_SMALL_Damage
 
                 );
             } else if ((type>=0.6f)&&(type<0.8)){
@@ -114,10 +125,10 @@ public class EnemiesEmitter {
                         bulletRegion,
                         ENEMY_MEDIUM_BULLET_HEIGHT,
                         ENEMY_MEDIUM_BULLET_VY,
-                        ENEMY_MEDIUM_BULLET_DAMAGE*level,
+                        ENEMY_MEDIUM_BULLET_DAMAGE,
                         ENEMY_MEDIUM_RELOAD_INTERVAL,
                         ENEMY_MEDIUM_HEIGHT,
-                        ENEMY_MEDIUM_HP*level,
+                        ENEMY_MEDIUM_HP,
                         ENEMY_MEDIUM_Damage
 
                 );
@@ -131,7 +142,7 @@ public class EnemiesEmitter {
                         ENEMY_BIG_BULLET_DAMAGE,
                         ENEMY_BIG_RELOAD_INTERVAL,
                         ENEMY_BIG_HEIGHT,
-                        ENEMY_BIG_HP*level,
+                        ENEMY_BIG_HP,
                         ENEMY_BIG_Damage
 
                 );
@@ -146,7 +157,7 @@ public class EnemiesEmitter {
                         DROP_BOX_RELOAD_INTERVAL,
                         DROP_BOX_HEIGHT,
                         DROP_BOX__HP,
-                        DROP_BOX__Damage*level
+                        DROP_BOX__Damage
 
                 );
             }
@@ -162,6 +173,7 @@ public class EnemiesEmitter {
     }
 public void setToNewGame(){
         level = 1;
+        generateInterval=3f;
 }
 
 }
